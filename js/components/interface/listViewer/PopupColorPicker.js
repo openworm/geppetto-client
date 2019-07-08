d7babe98e02fddd6d8e796db950494541d10ab6b
Filippo Ledda <filippo@metacell.us>
2019-07-08 15:53:19 +0200

    Refined ListViewer

--- js/components/interface/listViewer/PopupColorPicker.js
+++ js/components/interface/listViewer/PopupColorPicker.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import reactCSS from 'reactcss';
 import { SketchPicker, CompactPicker, SwatchesPicker } from 'react-color';
 import BaseIconComponent from './BaseIconComponent';
 
@@ -15,41 +14,39 @@ export default class PopupColorPicker extends React.Component {
     this.Picker = this.props.picker ? this.props.picker : CompactPicker;
   }
 
-  handleClick = () => {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker })
-  };
+  handleClick () {
+    return this.setState({ displayColorPicker: !this.state.displayColorPicker });
+  }
 
-  handleClose = () => {
+  handleClose () {
     this.setState({ displayColorPicker: false });
-  };
-  handleChange = color => {
+  }
+
+  handleChange (color) {
     this.setState({ color: color.hex, displayColorPicker: false });
     this.props.action(color.hex);
-  };
+  }
 
   render () {
 
-    const styles = reactCSS({
-      'default': {
-       container: {
-         position: 'relative'
-       },
-        popover: {
-          position: 'absolute',
-          zIndex: '2',
-        },
-        
+    const styles = {
+      container: { position: 'relative' },
+      popover: {
+        position: 'absolute',
+        zIndex: '2',
+        right: 0
       },
-    });
+        
+    };
 
     return (
       <div style={ styles.container } >
-        <BaseIconComponent action={ this.handleClick } color={ this.state.color } icon="tint" />
+        <BaseIconComponent action={ () => this.handleClick() } color={ this.state.color } icon={this.props.icon ? this.props.icon : "tint"} />
 
         { this.state.displayColorPicker ? <div style={ styles.popover }>
-          <div style={ styles.cover } onClick={ this.handleClose }/>
+          <div style={ styles.cover } onClick={ () => this.handleClose() }/>
           {
-             React.createElement(this.Picker, { color: this.state.color, onChange: this.handleChange })
+            React.createElement(this.Picker, { color: this.state.color, onChange: color => this.handleChange(color) })
           }
         </div> : null }
 
