716d26073b33ddb393f97f095d99f539b6c3e03e
jrmartin <yeshuarmartinez@gmail.com>
2021-12-22 09:46:51 -0800

    Bind functions before using them

--- geppetto-client/js/components/interface/3dCanvas/ThreeDEngine.js
+++ geppetto-client/js/components/interface/3dCanvas/ThreeDEngine.js
@@ -67,6 +67,14 @@ define(['jquery'], function () {
       this.linesUserPreference = undefined;
       this.hoverListeners = undefined;
       this.THREE = THREE;
+      
+      this.setupCamera = this.setupCamera().bind(this);
+      this.setupRenderer = this.setupRenderer().bind(this);
+      this.setupLights = this.setupLights().bind(this);
+      this.setupControls = this.setupControls().bind(this);
+      this.setupListeners = this.setupListeners().bind(this);
+      this.animate = this.animate.bind(this)();
+      
       // Initialisation
       this.setupCamera();
       this.setupRenderer();
