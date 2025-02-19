8dda5decd8b1b5b90e579c32f7a44cb6ae24bfa7
ddelpiano <dario@metacell.us>
2025-02-19 13:25:04 +0100

    purging not used component which gives problem with the shortid library

--- geppetto-client/js/components/ComponentFactory.js
+++ geppetto-client/js/components/ComponentFactory.js
@@ -13,7 +13,6 @@ define(function (require) {
 
       // All the components potentially instantiable go here
       components: {
-        'FORM': 'interface/form/Form',
         'PANEL': 'controls/panel/Panel',
         'LOGO': 'interface/logo/Logo',
         'LOADINGSPINNER': 'interface/loadingSpinner/LoadingSpinner',
@@ -179,7 +178,6 @@ define(function (require) {
               resolve(renderedComponent);
             }
             switch (componentType){
-            case 'FORM': require(['./interface/form/Form'], cb); break;
             case 'PANEL': require(['./controls/panel/Panel'],cb); break; 
             case 'LOGO': require(['./interface/logo/Logo'],cb); break;
             case 'LOADINGSPINNER': require(['./interface/loadingSpinner/LoadingSpinner'],cb); break;
