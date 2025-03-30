bdfe6a9299b7d2e8d2b80d54c50f79bedae750f6
Rob Court <robbie.court@gmail.com>
2025-03-30 15:48:50 +0100

    Revert "set count in query builder before processing results"
    This reverts commit 7dea726f26ad810d664e6d6af3fbba524bb7f177.

--- geppetto-client/js/components/interface/query/queryBuilder.js
+++ geppetto-client/js/components/interface/query/queryBuilder.js
@@ -788,8 +788,6 @@ define(function (require) {
           var compoundId = this.getCompoundQueryId(this.props.model.items);
           var match = false;
 
-          this.setCount(this.props.model.count);
-
           for (var i = 0; i < this.props.model.results.length; i++) {
             if (this.props.model.results[i].id == compoundId) {
               match = true;
