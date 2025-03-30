c6151a09ee8ac2652e2b4ce3cb51499e0e18fff3
Rob Court <robbie.court@gmail.com>
2025-03-30 12:39:05 +0100

    Revert "add debug log for error messages in query builder"
    This reverts commit a62b776c1decbf172f79f1f29ffb71a83f247822.

--- geppetto-client/js/components/interface/query/queryBuilder.js
+++ geppetto-client/js/components/interface/query/queryBuilder.js
@@ -1077,7 +1077,6 @@ define(function (require) {
     }
 
     setErrorMessage (message) {
-      console.debug('Query Builder error message:', message);
       this.setState({ errorMsg: message });
     }
 
